21dba6ca26fe1640a72bf53a1182d43b0fb1a1ea
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-01-22 01:13:35 +0530

    Add 'is prime'

---
+++ is-prime.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(define (is-prime num)
+  (define i 2)
+  (let loop()
+    (when (> (modulo num i) 0)
+      (set! i (+ i 1))
+      (loop)
+      ))
+  (= i num)
+  )
+
+(is-prime 2)
